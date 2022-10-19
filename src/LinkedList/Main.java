f77a66f2cdf586e1e2832e22b172c688a846d0b4
Raqibur-Rahman <raqibur.a@gmail.com>
2022-10-19 09:23:33 +0600

    LinkedList implementation...

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(12);
+        list.insert(33);
+        list.insert(99);
+        list.show();
+    }
+}
